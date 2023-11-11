4d5652919f57a936acdf826dda42813cd519e65c
Harald Kriebisch <haraldkriebisch@MBP-von-Harald.fritz.box>
2023-11-11 22:19:24 +0100

    Update profile page

--- hosting/src/pages/Main/Profile/index.tsx
+++ hosting/src/pages/Main/Profile/index.tsx
@@ -1,13 +1,58 @@
+import {Button} from "antd";
+import {AppPageWrapper, CenterInline} from "../../../components/StyledComponents";
+import {useTranslation} from "react-i18next";
 import styled from "styled-components";
-import {Outlet} from "react-router-dom";
+import {MailOutlined, UserOutlined} from "@ant-design/icons";
+import ContentCard from "../../../components/ContentCard";
+import {useNavigate} from "react-router-dom";
 
-const Wrapper = styled.div``;
+const Extra = styled.div`
+  flex-grow: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  gap: 20px;
+`;
 
 const Profile = () => {
+    const {t} = useTranslation();
+    const navigate = useNavigate();
+
     return (
-        <Wrapper data-cy={'profile-page'}>
-            <Outlet/>
-        </Wrapper>
+        <AppPageWrapper data-cy={'profile-page'}>
+            <CenterInline>
+                <Button type={"primary"} size={"large"} ghost={true} title={t("Export to Google Sheets")}
+                        data-cy="data-export-button">
+                    {t("Export to Google Sheets")}
+                </Button>
+            </CenterInline>
+            <CenterInline>
+                <Button type={"primary"} danger={true} size={"large"} ghost={true} title={t("Logout")}
+                        data-cy="logout-button">
+                    {t("Logout")}
+                </Button>
+            </CenterInline>
+            <Extra>
+                <ContentCard>
+                    <span>
+                        {t("In the case that you have a feature request you may use the form to contact me directly.")}
+                    </span>
+                    <CenterInline>
+                        <Button type={"link"} size={"large"} ghost={true} title={t("Contact")}
+                                icon={<MailOutlined/>} onClick={() => navigate("/legal/contact")}
+                                data-cy="contact-button">
+                            {t("Contact")}
+                        </Button>
+                    </CenterInline>
+                </ContentCard>
+                <CenterInline>
+                    <Button type={"text"} danger={true} size={"large"} title={t("Delete Profile")}
+                            icon={<UserOutlined/>} data-cy="delete-profile-button">
+                        {t("Delete Profile")}
+                    </Button>
+                </CenterInline>
+            </Extra>
+        </AppPageWrapper>
     );
 };
 
