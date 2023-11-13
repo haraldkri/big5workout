e39adecb382f7dcf7f321e16ab7fd5fb92febcfb
Harald Kriebisch <haraldkriebisch@MBP-von-Harald.fritz.box>
2023-11-13 08:40:00 +0100

    Add default exercises config

---
+++ hosting/src/static/defaultExercises.ts
@@ -0,0 +1,77 @@
+export default [
+    {
+        key: 'machine-chest-press',
+        germanName: 'Brustpresse',
+        englishName: 'Chest Press',
+        images: [
+            {
+                url: "https://modusx.de/wp-content/uploads/brustpresse-mittlere-brust.jpg",
+                link: "https://modusx.de/fitness-uebungen/brustpresse/brustpresse-mit-fokus-auf-die-mittlere-brust/"
+            },
+            {
+                url: "https://modusx.de/wp-content/uploads/brustpresse-mit-gewichtsscheiben.gif",
+                link: "https://modusx.de/fitness-uebungen/brustpresse/brustpresse-mit-fokus-auf-die-mittlere-brust/"
+            }
+        ],
+    },
+    {
+        key: 'machine-shoulder-press',
+        germanName: 'Schulterdrücken',
+        englishName: 'Shoulder Press',
+        images: [
+            {
+                url: "https://modusx.de/wp-content/uploads/schulterdruecken-an-der-maschine.jpg",
+                link: "https://modusx.de/fitness-uebungen/schulterdruecken-an-maschine/"
+            },
+            {
+                url: "https://modusx.de/wp-content/uploads/schulterdruecken-maschine.gif",
+                link: "https://modusx.de/fitness-uebungen/schulterdruecken-an-maschine/"
+            }
+        ],
+    },
+    {
+        key: 'machine-seated-row',
+        germanName: 'Rudern',
+        englishName: 'Seated Row',
+        images: [
+            {
+                url: "https://modusx.de/wp-content/uploads/kabelrudern-mit-breitem-griff.jpg",
+                link: "https://modusx.de/fitness-uebungen/rudern-am-kabelzug-sitzend/kabelrudern-mit-breitem-griff/"
+            },
+            {
+                url: "https://modusx.de/wp-content/uploads/kabelrudern-mit-breitem-griff-hufgriff.gif",
+                link: "https://modusx.de/fitness-uebungen/rudern-am-kabelzug-sitzend/kabelrudern-mit-breitem-griff/"
+            }
+        ],
+    },
+    {
+        key: 'machine-pull-down',
+        germanName: 'Latzug',
+        englishName: 'Pull Down',
+        images: [
+            {
+                url: "https://modusx.de/wp-content/uploads/klassische-latziehen-zur-brust-1080x608.jpg",
+                link: "https://modusx.de/fitness-uebungen/latziehen-zur-brust/klassische-latziehen-zur-brust/"
+            },
+            {
+                url: "https://modusx.de/wp-content/uploads/klassische-latziehen-zur-brust.gif",
+                link: "https://modusx.de/fitness-uebungen/latziehen-zur-brust/klassische-latziehen-zur-brust/"
+            }
+        ],
+    },
+    {
+        key: 'machine-leg-press',
+        germanName: 'Beinpresse',
+        englishName: 'Leg Press',
+        images: [
+            {
+                url: "https://modusx.de/wp-content/uploads/beinpresse-mit-beiden-beinen.jpg",
+                link: "https://modusx.de/fitness-uebungen/beinpresse-klassisch/beinpresse-mit-beiden-beinen/"
+            },
+            {
+                url: "https://modusx.de/wp-content/uploads/beinpresse-mit-beiden-beinen-eng.gif",
+                link: "https://modusx.de/fitness-uebungen/beinpresse-klassisch/beinpresse-mit-beiden-beinen/"
+            }
+        ],
+    },
+]
