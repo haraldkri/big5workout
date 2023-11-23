172cd1e722f2ae58368512510f0163bd6fa30c09
Harald Kriebisch <haraldkriebisch@MBP-von-Harald.fritz.box>
2023-11-23 23:45:33 +0100

    Add custom types

--- hosting/src/types.ts
+++ hosting/src/types.ts
@@ -3,3 +3,20 @@ import {ReactNode} from "react";
 export type PropsWithChildren<P = unknown> = P & { children: ReactNode };
 
 export type PropsWithOptionalChildren<P = unknown> = P & { children?: ReactNode };
+
+export type ExerciseValue = {
+    weight?: number,
+    duration?: number,
+}
+
+export type Workout = {
+    germanName: string,
+    englishName: string,
+    exerciseIds: string[],
+    createdAt: number,
+    lastUsed: number,
+}
+
+export type WorkoutWithId = Workout & {
+    id: string,
+}
