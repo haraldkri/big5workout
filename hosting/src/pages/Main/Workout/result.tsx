ecc34dcf669d3beaff6d1373e65a4ec337c55bd8
Harald Kriebisch <haraldkriebisch@MacBook-Pro-von-Harald.local>
2023-12-08 13:47:48 +0100

    Add workout result page

--- hosting/src/pages/Main/Workout/result.tsx
+++ hosting/src/pages/Main/Workout/result.tsx
@@ -1,13 +1,27 @@
 import styled from "styled-components";
-import {Outlet} from "react-router-dom";
+import {useTranslation} from "react-i18next";
+import {Button, Result} from "antd";
+import {useNavigate} from "react-router-dom";
 
 const Wrapper = styled.div``;
 
 const WorkoutResult = () => {
+    const {t} = useTranslation();
+    const navigate = useNavigate();
     return (
         <Wrapper data-cy={'workout-result-page'}>
-            workout result page
-            <Outlet/>
+            <Result
+                status="success"
+                title={t("Successfully Saved Workout!")}
+                subTitle={t("The recorded weights and durations will be displayed for every exercise the next time you start a workout.")}
+                extra={[
+                    <Button type="primary" key="console" ghost={true}
+                            onClick={() => navigate(`/app`)}
+                    >
+                        {t('Start Workout')}
+                    </Button>
+                ]}
+            />
         </Wrapper>
     );
 };
