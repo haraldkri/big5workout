2ee9e735daab24698b16f657c43c87add4e61320
Harald Kriebisch <haraldkriebisch@MBP-von-Harald.fritz.box>
2024-01-06 21:10:04 +0100

    Update getName (fallback language keys)

--- hosting/src/utils/languageKeySelect.ts
+++ hosting/src/utils/languageKeySelect.ts
@@ -1,8 +1,8 @@
 // Please provide the function with the language object from the local i18n instance
 // const {i18n} = useTranslation();
 // i18n.language
-export function getName(data: Required<{ name: { de: string, en: string } }> | null, language: string) {
+export function getName(data: Required<{ name: { de?: string, en?: string } }> | null, language: string) {
     if (!data) return undefined;
-    if (language === "de-DE") return data?.name.de;
-    return data?.name.en;
+    if (language === "de-DE") return data?.name.de || data?.name.en;
+    return data?.name.en || data?.name.de;
 }
